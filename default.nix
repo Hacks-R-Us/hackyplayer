abc96d85419df0e4cb7004aa5c240b82156c8a65
Luke Granger-Brown <git@lukegb.com>
2024-06-12 22:53:40 +0100

    nix: switch to ffmpeg-full and provide a fontconfig file to ensure Raleway is available

--- default.nix
+++ default.nix
@@ -17,16 +17,29 @@ let
       });
     });
   };
+  ffmpeg = pkgs.ffmpeg-full;  # we need librsvg support
+
+  fontconfigConf = pkgs.makeFontsConf {
+    fontDirectories = [
+      (lib.sources.sourceFilesBySuffices ./hackyplayer/resources [ ".ttf" ])
+    ];
+  };
+  ffmpegWrapper = pkgs.writeShellScript "ffmpeg-wrapper" ''
+    export FONTCONFIG_FILE="${fontconfigConf}"
+    exec "${lib.getExe' ffmpeg "ffmpeg"}" "$@"
+  '';
 in
-  hackyplayer.dependencyEnv.override {
+  (hackyplayer.dependencyEnv.override {
     app = hackyplayer.overridePythonAttrs (old: {
       postPatch = ''
         ${old.postPatch or ""}
 
         substituteInPlace hackyplayer/formvideo.py \
-          --replace-fail 'FFMPEG_BIN = "ffmpeg"' 'FFMPEG_BIN = "${lib.getExe' pkgs.ffmpeg "ffmpeg"}"' \
+          --replace-fail 'FFMPEG_BIN = "ffmpeg"' 'FFMPEG_BIN = "${ffmpegWrapper}"' \
           --replace-fail 'IMAGEMAGICK_BIN = "convert"' 'IMAGEMAGICK_BIN = "${lib.getExe' pkgs.imagemagick "convert"}"' \
           --replace-fail 'APP_ROOT = Path(".")' 'APP_ROOT = Path("${placeholder "out"}/${hackyplayer.python.sitePackages}/hackyplayer")'
       '';
     });
+  }) // {
+    inherit fontconfigConf ffmpegWrapper;
   }
